3523205adebb97473ea72c4d91d86ba7f7ebb163
Bunn Kay <top2541top@gmail.com>
2017-09-08 01:35:14 +0700

    Create ex3.py

---
+++ code/lecture/ex3.py
@@ -0,0 +1,21 @@
+count = 0
+total = 0
+startCount = 0
+storage = {}
+
+while True:
+  
+  x = int(input())
+  check = input('Next value (y/n)? ')
+  
+  storage[count] = x
+  count += 1  
+  
+  if check == 'n':
+    if count < 2:
+      print('error')
+    break
+  
+startCount = count - 2
+total = storage[startCount] + storage[startCount + 1]
+print('Sum is %d' %total)
